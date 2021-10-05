e77d7019ffb674840419314dde5415fc230e8d3a
Alvaro Starling Hernandez Luzon <alvarostarling24@gmail.com>
2021-10-04 22:04:49 -0400

    Creando Diseno del registro de Aportes y anadiendo recursos

---
+++ UI/Registros/rAportes.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace P1_AP1_Alvaro_20190269.UI.Registros
+{
+    /// <summary>
+    /// Interaction logic for rAportes.xaml
+    /// </summary>
+    public partial class rAportes : Window
+    {
+        public rAportes()
+        {
+            InitializeComponent();
+        }
+
+        private void Buscar_Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Nuevo_Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Guardar_Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Eliminar_Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
